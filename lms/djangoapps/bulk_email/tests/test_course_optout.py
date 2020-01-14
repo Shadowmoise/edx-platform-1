roblhibbard/edx-platform

Unsubscribe link added in bulk emails

lms/djangoapps/bulk_email/tests/test_course_optout.py
@@ -5,6 +5,7 @@ Unit tests for student optouts from course email
 from __future__ import absolute_import
 
 import json
+import re
 
 from django.core import mail
 from django.core.management import call_command
@@ -86,6 +87,54 @@ class TestOptoutCourseEmails(ModuleStoreTestCase):
         # Assert that self.student.email not in mail.to, outbox should only contain "myself" target
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].to[0], self.instructor.email)
+
+    def test_optout_using_unsubscribe_link_in_email(self):
+        """
+        Make sure email (with Unicode characters) send to all goes there.
+        """
+        self.client.logout()
+
+        self.client.login(username=self.instructor.username, password="test")
+        self.navigate_to_email_view()
+
+        test_email = {
+            'action': 'Send email',
+            'send_to': '["learners"]',
+            'subject': 'Checking unsubscribe link in email',
+            'message': 'test message for all'
+        }
+        response = self.client.post(self.send_mail_url, test_email)
+        self.assertEquals(json.loads(response.content.decode('utf-8')), self.success_content)
+
+        # check unsubscribe link in template
+        for m in mail.outbox:
+            plain_template = m.body
+            html_template = m.alternatives[0][0]
+
+            assert u'bulk_email/email/optout/' in plain_template
+            assert u'bulk_email/email/optout/' in html_template
+
+        unsubscribe_link = re.findall(r"""/bulk_email.*?=[^']""", mail.outbox[0].alternatives[0][0])[0].strip()
+        response = self.client.get(unsubscribe_link+'/')
+
+        self.assertContains(response, 'unsubscribe')
+
+        response = self.client.post(unsubscribe_link+'/', {'unsubscribe': 'On'})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'You are successfully unsubscribed.')
+
+        self.client.logout()
+
+        test_email = {
+            'action': 'Send email',
+            'send_to': '["myself", "staff", "learners"]',
+            'subject': 'Checking unsubscribe link in email',
+            'message': 'test message for all'
+        }
+        response = self.client.post(self.send_mail_url, test_email)
+        self.assertEquals(json.loads(response.content.decode('utf-8')), self.success_content)
+        self.assertEqual(len(mail.outbox), 1)
 
     def test_optin_course(self):
         """

lms/djangoapps/bulk_email/tests/test_email.py
@@ -6,6 +6,7 @@ from __future__ import absolute_import
 
 import json
 import os
+import re
 from unittest import skipIf
 
 import ddt
@@ -603,6 +604,28 @@ class TestEmailSendFromDashboardMockedHtmlToText(EmailSendFromDashboardTestCase)
                                 [s.email for s in added_users if s not in optouts])
         six.assertCountEqual(self, outbox_contents, should_send_contents)
 
+    def test_unsubscribe_link_in_email(self):
+        """
+        Make sure email (with Unicode characters) send to all goes there.
+        """
+
+        test_email = {
+            'action': 'Send email',
+            'send_to': '["learners"]',
+            'subject': 'Checking unsubscribe link in email',
+            'message': 'test message for all'
+        }
+        response = self.client.post(self.send_mail_url, test_email)
+        self.assertEquals(json.loads(response.content.decode('utf-8')), self.success_content)
+
+        # check unsubscribe link in template
+        for m in mail.outbox:
+            plain_template = m.body
+            html_template = m.alternatives[0][0]
+
+            assert u'bulk_email/email/optout/' in plain_template
+            assert u'bulk_email/email/optout/' in html_template
+
 
 @skipIf(os.environ.get("TRAVIS") == 'true', "Skip this test in Travis CI.")
 class TestEmailSendFromDashboard(EmailSendFromDashboardTestCase):
@@ -694,3 +717,4 @@ class TestCourseEmailContext(SharedModuleStoreTestCase):
         """
         email_context = _get_course_email_context(self.course)
         self.verify_email_context(email_context, 'https')
+

Shadowmoise/edx-platform-1

lms/djangoapps/bulk_email/tests/test_course_optout.py
@@ -5,6 +5,7 @@ Unit tests for student optouts from course email
 
 
 import json
+import re
 
 from django.core import mail
 from django.core.management import call_command
@@ -86,6 +87,52 @@ class TestOptoutCourseEmails(ModuleStoreTestCase):
         # Assert that self.student.email not in mail.to, outbox should only contain "myself" target
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].to[0], self.instructor.email)
+
+    def test_optout_using_unsubscribe_link_in_email(self):
+        """
+        Make sure email (with Unicode characters) send to all goes there.
+        """
+        self.client.logout()
+
+        self.client.login(username=self.instructor.username, password="test")
+        self.navigate_to_email_view()
+
+        test_email = {
+            'action': 'Send email',
+            'send_to': '["learners"]',
+            'subject': 'Checking unsubscribe link in email',
+            'message': 'test message for all'
+        }
+        response = self.client.post(self.send_mail_url, test_email)
+        self.assertEqual(json.loads(response.content.decode('utf-8')), self.success_content)
+
+        # check unsubscribe link in template
+        plain_template = mail.outbox[0].body
+        html_template = mail.outbox[0].alternatives[0][0]
+
+        assert u'bulk_email/email/optout/' in plain_template
+        assert u'bulk_email/email/optout/' in html_template
+
+        unsubscribe_link = re.findall(r"""/bulk_email/email.*(?='>)""", mail.outbox[0].alternatives[0][0])[0].strip()
+
+        response = self.client.get(unsubscribe_link)
+
+        self.assertContains(response, 'unsubscribe')
+
+        response = self.client.post(unsubscribe_link, {'unsubscribe': True})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'You have successfully unsubscribed from')
+
+        test_email = {
+            'action': 'Send email',
+            'send_to': '["staff", "learners"]',
+            'subject': 'Checking unsubscribe link in email',
+            'message': 'test message for all'
+        }
+        response = self.client.post(self.send_mail_url, test_email)
+        self.assertEqual(json.loads(response.content.decode('utf-8')), self.success_content)
+        self.assertEqual(len(mail.outbox), 1)
 
     def test_optin_course(self):
         """

lms/djangoapps/bulk_email/tests/test_email.py
@@ -24,6 +24,7 @@ from mock import Mock, patch
 from bulk_email.models import BulkEmailFlag, Optout
 from bulk_email.tasks import _get_course_email_context, _get_source_address
 from course_modes.models import CourseMode
+
 from lms.djangoapps.courseware.tests.factories import InstructorFactory, StaffFactory
 from lms.djangoapps.instructor_task.subtasks import update_subtask_status
 from openedx.core.djangoapps.course_groups.cohorts import add_user_to_cohort
@@ -602,6 +603,28 @@ class TestEmailSendFromDashboardMockedHtmlToText(EmailSendFromDashboardTestCase)
                                 [s.email for s in self.students] +
                                 [s.email for s in added_users if s not in optouts])
         six.assertCountEqual(self, outbox_contents, should_send_contents)
+
+    def test_unsubscribe_link_in_email(self):
+        """
+        Make sure email (with Unicode characters) send to all goes there.
+        """
+
+        test_email = {
+            'action': 'Send email',
+            'send_to': '["learners"]',
+            'subject': 'Checking unsubscribe link in email',
+            'message': 'test message for all'
+        }
+        response = self.client.post(self.send_mail_url, test_email)
+        self.assertEqual(json.loads(response.content.decode('utf-8')), self.success_content)
+
+        # check unsubscribe link in template
+        for m in mail.outbox:
+            plain_template = m.body
+            html_template = m.alternatives[0][0]
+
+            assert u'bulk_email/email/optout/' in plain_template
+            assert u'bulk_email/email/optout/' in html_template
 
 
 @skipIf(os.environ.get("TRAVIS") == 'true', "Skip this test in Travis CI.")
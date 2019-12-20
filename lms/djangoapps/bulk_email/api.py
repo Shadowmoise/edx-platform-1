56f078d15f3edf6c0f853d5587a219cdf0a3f3b6
adeelehsan <adeel.ehsan@arbisoft.com>
2019-12-20 09:59:06 +0500

    Unsubscribe link added in bulk emails

--- lms/djangoapps/bulk_email/api.py
+++ lms/djangoapps/bulk_email/api.py
@@ -6,11 +6,22 @@ Python APIs exposed by the bulk_email app to other in-process apps.
 # Public Bulk Email Functions
 
 
+import logging
+
 from bulk_email.models_api import (
     is_bulk_email_enabled_for_course,
     is_bulk_email_feature_enabled,
     is_user_opted_out_for_course
 )
+
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from lms.djangoapps.discussion.notification_prefs.views import UsernameCipher
+
+User = get_user_model()
+log = logging.getLogger(__name__)
 
 
 def get_emails_enabled(user, course_id):
@@ -28,3 +39,16 @@ def get_emails_enabled(user, course_id):
     if is_bulk_email_feature_enabled(course_id=course_id):
         return not is_user_opted_out_for_course(user=user, course_id=course_id)
     return None
+
+
+def get_unsubscribed_link(username, course_id):
+    """
+
+    :param username: username
+    :param course_id:
+    :return: AES encrypted token based on the user email
+    """
+    token = UsernameCipher.encrypt(username)
+    optout_url = reverse('bulk_email_opt_out', kwargs={'token': token, 'course_id': course_id})
+    url = '{base_url}{optout_url}'.format(base_url=settings.LMS_ROOT_URL, optout_url=optout_url)
+    return url
